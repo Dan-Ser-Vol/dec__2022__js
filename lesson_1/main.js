2096cb77a2ff2f8f7ec14e5fba3a93d3a0ec0c5c
Sergey <62606319+Dan-Ser-Vol@users.noreply.github.com>
2023-01-31 23:45:34 +0300

    added homework 1

---
+++ lesson_1/main.js
@@ -0,0 +1,33 @@
+//Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ.
+let hello = 'hello'
+let owu = 'owu'
+let com = 'com'
+let ua = 'ua'
+let num1 = 1
+let num10 = 10
+let numMinus999 = -999
+let num123 = 123
+const PI = 3.14
+const NUM_FLOAT = 2.7
+const NUM_16 = 16
+let isTrue = true
+let isFalse = false
+
+//Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ.
+let firstName = 'Sergiy'
+let middleName = 'Volodymyrovych'
+let lastName = 'Danyliuk'
+let person = `${lastName} ${firstName} ${middleName}`
+console.log(person);
+
+
+//За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+let a = 100;  let b = '100';  let c = true;
+console.log(typeof  a, b, c)
+
+
+//- За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+  let myName = prompt("Введіть ім'я", '')
+  let fatherName = prompt("Введіть Як Вас Побатькові", '')
+  let age = prompt("Введіть ваш вік", '')
+console.log(`${myName} ${fatherName} ${age}`)
