d48d42e0a20183aee998fdd9b42fd7263c8072a7
Sergey <62606319+Dan-Ser-Vol@users.noreply.github.com>
2023-02-01 00:01:54 +0300

    added homework

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,4 +1,4 @@
-//Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ.
+//- Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
 let hello = 'hello'
 let owu = 'owu'
 let com = 'com'
