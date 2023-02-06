765e701ee2c45f36803adac317fbd0c7ed2cc828
Sergey <62606319+Dan-Ser-Vol@users.noreply.github.com>
2023-02-06 17:05:39 +0300

    fixed homework 2

--- lesson_2/main.js
+++ lesson_2/main.js
@@ -13,14 +13,14 @@ console.log(arr[8]);
 console.log(arr[9]);
 
 // - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
-let book1 = {title: "First Book", pageCount: 100, genre: 'fantasy'};
-let book2 = {title: "Second Book", pageCount: 266, genre: 'drama'};
-let book3 = {title: "Third", pageCount: 399, genre: 'action'}
+let book1 = {title: 'First Book', pageCount: 100, genre: 'fantasy'};
+let book2 = {title: 'Second Book', pageCount: 266, genre: 'drama'};
+let book3 = {title: 'Third', pageCount: 399, genre: 'action'}
 
 // - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - являється  масивом. Кожен автор має поля name та age.
-let book4 = {title: "First Book", pageCount: 100, genre: 'fantasy', authors: [{name: 'Peter', age: 34}]};
-let book5 = {title: "Second Book", pageCount: 266, genre: 'drama', authors: [{name: 'John', age: 23}]};
-let book6 = {title: "third Book", pageCount: 399, genre: 'action', authors: [{name: 'Lena', age: 67}]};
+let book4 = {title: 'First Book', pageCount: 100, genre: 'fantasy', authors: [{name: 'Peter', age: 34}]};
+let book5 = {title: 'Second Book', pageCount: 266, genre: 'drama', authors: [{name: 'John', age: 23}]};
+let book6 = {title: 'third Book', pageCount: 399, genre: 'action', authors: [{name: 'Lena', age: 67}]};
 
 // - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
 const users = [
@@ -50,18 +50,17 @@ console.log(`user password: ${users[9]['password']}`);
 //     - Є змінна х, якій ви надаєте довільне числове значення.
 //     Якщо змінна x не дорівнює нулю, виведіть 'Вірно', інакше виведіть 'Невірно'. Перевірте  скрипт при a, що дорівнює 1, 0, -3
 let x = -3;
-x !== 0 ? console.log("Вірно") : console.log("Невірно");
-
+x !== 0 ? console.log('Вірно') : console.log('Невірно');
 // - Дано змінну time яка рівна числу від 0 до 59. Потрібно написати код, який перевірить, до якої четверті години попадає число
 // (в першу, другу, третю или четверту частину години).
 const time = 33;
-if ((time => 0) && (time <= 14)) {
+if (time <= 15) {
     console.log(` ${time} попадає в першу чверть години`);
-} else if ((time => 15) && (time <= 29)) {
+} else if (time <= 30) {
     console.log(` ${time} попадає в другу чверть години`);
-} else if ((time => 30) && (time <= 44)) {
+} else if (time <= 45) {
     console.log(` ${time} попадає в третю чверть години`);
-} else if ((time => 45) && (time <= 59)) {
+} else if (time <= 59) {
     console.log(` ${time} попадає в четверту чверть години`);
 } else {
     console.log(`Ви ввели некоректне значення! Введіть число від 0 до 59`);
@@ -70,11 +69,11 @@ if ((time => 0) && (time <= 14)) {
 
 // - У змінній day дано якесь число від 1 до 31. Потрібно визначити, у яку половину(декаду) місяця потрапляє це число (у першу, другу чи третю).
 let day = 23;
-if ((day => 1) && (day <= 10)) {
+if ((day >= 1) && (day <= 10)) {
     console.log(` ${day} попадає в першу декаду місяця`);
-} else if ((day => 11) && (day <= 20)) {
+} else if ((day >= 11) && (day <= 20)) {
     console.log(`${day} попадає в другу декаду місяця`);
-} else if ((day => 21) && (day <= 31)) {
+} else if ((day >= 21) && (day <= 31)) {
     console.log(` ${day} попадає в третю декаду місяця`);
 } else {
     console.log(`Ви ввели некоректне значення! Введіть число від 0 до 31`);
@@ -82,31 +81,31 @@ if ((day => 1) && (day <= 10)) {
 
 
 // - Скласти розклад на тиждень за домопоги switch. Користувач вводить порядковий номер дня тижня і на екрані відображається інфа що заплановано на цей день (можна замість плану на день, назву дня англійською).
-let i = parseInt(prompt("Введіть порядковий номер дня тижня напрклад  для Понеділок 1"));
+let i = parseInt(prompt('Введіть порядковий номер дня тижня напрклад  для Понеділок 1'));
 switch (i) {
     case 1 :
-        alert("Monday");
+        alert('Monday');
         break;
     case 2 :
-        alert("Tuesday");
+        alert('Tuesday');
         break;
     case 3 :
-        alert("Wednesday");
+        alert('Wednesday');
         break;
     case 4 :
-        alert("Thursday");
+        alert('Thursday');
         break;
     case 5 :
-        alert("Friday");
+        alert('Friday');
         break;
     case 6 :
-        alert("Saturday");
+        alert('Saturday');
         break;
     case 7 :
-        alert("Sunday");
+        alert('Sunday');
         break;
     default:
-        alert("Ви ввели неправильне значення");
+        alert('Ви ввели неправильне значення');
 }
 
 //     - Користувач вводить або має два числа.
@@ -123,18 +122,25 @@ if (a > b) {
 }
 
 //
-//     - є змінна х, яка може прийняти будь-яке значення (стрінг, число, undefined, null  і тд включно). Напишіть код який,
+// - є змінна х, яка може прийняти будь-яке значення (стрінг, число, undefined, null  і тд включно). Напишіть код який,
+//за допомоги  оператора || буде присвоювати змінній х значення "default"  якщо значення змінної х являється falsy (хибноподібні, тобто приводиться до false)
+
+
+let x = NaN
+x = x || 'default'
+console.log(x)
+
 //
-//        за допомоги  оператора || буде присвоювати змінній х значення "default"  якщо значення змінної х являється falsy (хибноподібні, тобто приводиться до false)
+// let x = 1
+// !!x === false? console.log(x = 'default') : console.log(x)
 
-let x2 = undefined;
-if (!!x2 === false || !!x2 === 0 ||!!x2 === '' || !!x2 === null ||!!x2 === undefined ||!!x2 === NaN ||!!x2 === -0) {
-    x2 = 'default';
-    console.log(x2);
-} else {
-    console.log(x2);
-}
-
+// let x2 = undefined;
+// if (!!x2 === false || !!x2 === 0 || !!x2 === '' || !!x2 === null || !!x2 === undefined || !!x2 === NaN || !!x2 === -0) {
+//     x2 = 'default';
+//     console.log(x2);
+// } else {
+//     console.log(x2);
+// }
 
 
 //     - з файлу arrays.js (лежить в папці 2023 plan ) взяти масив coursesAndDurationArray. За допомоги іф перевірити кожен його елемент на тривалість навчання. У випадку якщо тривалість довша за 5 місяців вивести в консоль "Супер".
