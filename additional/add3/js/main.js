305921f03d7b5d21bdf29bae55edd6ed919aecee
Sergey <62606319+Dan-Ser-Vol@users.noreply.github.com>
2023-02-08 21:57:31 +0300

    homework 3 fix

--- additional/add3/js/main.js
+++ additional/add3/js/main.js
@@ -1,44 +1,29 @@
 const users = window.users
 //     /*За допомогою циклу вивести всю інформацію про пости використовуючи шаблон Template 1.1.
 //     * ЗА допомоги стилів побудувати структуру "сітки" по 5 об'єктів в лінію
-//     * */
 
-// <!--template 1.1-->
-// <div class="wrap">
-//     <div class="post-card">
-//         <h3>ID - TITLE</h3>
-//         <p>BODY</p>
-//     </div>
-//
-//     <!--
-//     ...
-//     ...
-//     ...
-//     other posts
-//     -->
-//
-// </div>
-
-for (const user of users) {
-    for (const userKey in user) {
-        document.write(`
-         <ul class="post-card">
-            <li> ${userKey}: ${user[userKey]}</li>
-         </ul>
+document.write(`<div class="wrap">`)
+for (let i = 0; i < users.length; i++) {
+    const user = users[i]
+    document.write(`
+         <div class="post-card">
+            <h3>  ID: ${user.id}  TITLE: ${user.title}}</h3>
+            <p>${user.body}</p>
+         </div>
         `)
-    }
 }
+document.write(`</div>`)
 
-// document.write(`<div class="wrap">`)
-// for (let i = 0; i < users.length; i++) {
-//     const user = users[i]
-//     document.write(`
-//          <div class="post-card">
-//             <h3>  ID: ${user.id}  TITLE: ${user.title}}</h3>
-//             <p>${user.body}</p>
-//          </div>
+
+
+// for (const user of users) {
+//     for (const userKey in user) {
+//         document.write(`
+//          <ul class="post-card">
+//             <li> ${userKey}: ${user[userKey]}</li>
+//          </ul>
 //         `)
+//     }
 // }
-// document.write(`</div>`)
 
 
