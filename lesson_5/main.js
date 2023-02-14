6b8a209014a256fc3b2fc2ea1ecc4e64ccc81b7f
Sergey <62606319+Dan-Ser-Vol@users.noreply.github.com>
2023-02-14 22:25:24 +0300

    homework 5

--- lesson_5/main.js
+++ lesson_5/main.js
@@ -1,70 +1,63 @@
+const PI = 3.14
 // 1) створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
-function rectangularAreaCalc(a, b) {
-    return a * b
-}
-
-console.log(rectangularAreaCalc(5, 4));
+const rectangularCalc = (a, b) => a * b
+console.log(rectangularCalc(5, 4));
 
 
 // 2) створити функцію яка обчислює та повертає площу кола з радіусом r
-function calcAreaCircle(r) {
-    const PI = 3.14
+const calcCircle = (r) => {
     return PI * (r ** 2)
 }
+console.log(calcCircle(5));
 
-console.log(calcAreaCircle(5));
 
 // 3)  створити функцію яка обчислює та повертає площу циліндру висотою h, та радіуом r
-function calcAreaCylinder(r, h) {
-    const PI = 3.14
+const calcCylinder = (r, h) => {
     return 2 * PI * r * (r + h)
-
 }
+console.log(calcCylinder(5, 1));
 
-console.log(calcAreaCylinder(5, 1));
 
 // 4) створити функцію яка приймає масив та виводить кожен його елемент
-const arr = [3, 45, 454, 23, 9, 6, 'hello']
-
-function showArrItem(arr) {
+const array = [3, 45, 454, 23, 9, 6, 'hello']
+const showArrItem = (arr) => {
     for (const arrElement of arr) console.log(arrElement);
 }
+showArrItem(array)
 
-showArrItem(arr)
 
 // 5) створити функцію яка створює параграф з текстом. Текст задати через аргумент
-function createText(text) {
+const createTextParagraph = (text) => {
     document.write(`<p class="old">${text}</p>`)
 }
+createTextParagraph('Some text')
 
-createText('Some text')
 
 // 6) створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
-function createListText(text) {
+const createList = (text) => {
     document.write(`<ul>
                         <li>${text}</li>
                         <li>${text}</li>
                         <li>${text}</li>
                     </ul>`)
 }
+createList('Some text for li')
 
-createListText('Some text for li')
 
 // 7) створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
-function createListIter(text, n) {
+const printList = (text, n) => {
     document.write(`<ul>`)
     for (let i = 0; i < n; i++) {
         document.write(`<li>${text}</li>`)
     }
     document.write(`<ul>`)
 }
+printList('Some text for iterator li ', 3)
 
-createListIter('Some text for iterator li ', 3)
 
 // 8) створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
-const arr2 = [3, 45, 23, 9, 6, 'hello', true,]
-
-function listIter(items) {
+const arr3 = [3, 45, 23, 9, 6, 'hello', true,]
+const listIter = (items) => {
     document.write(`<ul>`)
     for (let j = 0; j < items.length; j++) {
         const item = items[j];
@@ -72,11 +65,10 @@ function listIter(items) {
     }
     document.write(`<ul>`)
 }
-
-listIter(arr2)
+listIter(arr3)
 
 // 9) створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
-const usersItems = [
+const userItems = [
     {id: 1, name: 'Joo', age: 34},
     {id: 2, name: 'Bruce', age: 34,},
     {id: 3, name: 'Boo', age: 34},
@@ -85,14 +77,13 @@ const usersItems = [
     {id: 6, name: 'Jerry', age: 34,},
 ];
 
-function showUserElem(arr) {
+const showUserElem = (arr) => {
     for (let j = 0; j < arr.length; j++) {
         const item = arr[j];
         document.write(`<div class="user_green">ID: ${item.id} NAME: ${item.name} AGE: ${item.age}</divcla>`)
     }
 }
-
-showUserElem(usersItems)
+showUserElem(userItems)
 
 // 10) створити функцію яка повертає найменьше число з масиву
 const numArr = [7, 1, 8, 2, 9, 10, 4, 5, 6, 3,]
@@ -109,41 +100,34 @@ console.log(showMinNumber(numArr))
 
 
 // 11) створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
-function sum(arr) {
+const sum = (arr) => {
     let res = 0
     for (const arrElement of arr) {
-        res = res + arrElement
+        res += arrElement
     }
     return res
 }
+console.log('sum', sum(numArr))
 
-console.log(sum(numArr))
 
 // 12) створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
 // Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
 const arrSwap = [1, 2, 3, 4, 6, 7, 8, 9]
-
-function swap(arr, index1, index2) {
-    let a = arr[index1]
-    let b = arr[index2]
-    arr[index1] = a
-    arr[index2] = b
+const swap = (arr, idx1, idx2) => {
+    [arr[idx2], arr[idx1]] = [arr[idx1], arr[idx2]]
     return arr
 }
-
 console.log(swap(arrSwap, 0, 7))
 
 
 // 13) Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 // Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
 const currencyArr = [{currency: 'USD', value: 40}, {currency: 'EUR', value: 42}]
-
-function exchange(sumUAH, currencyValues, exchangeCurrency) {
+const exchange = (sumUAH, currencyValues, exchangeCurrency) => {
     for (const currencyElem of currencyValues) {
         if (currencyElem.currency === exchangeCurrency) {
             return sumUAH / currencyElem.value
         }
     }
 }
-
 console.log(exchange(10000, currencyArr, 'USD'));
